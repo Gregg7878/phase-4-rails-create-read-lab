e923ca7a08c27ae680147ff6190b153d57aa2a4b
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-08-13 12:13:07 +0300

    modified plant_controller.rb

--- app/controllers/plant_controller.rb
+++ app/controllers/plant_controller.rb
@@ -1,24 +1,26 @@
 class PlantsController < ApplicationController
-  before_action :set_plant, only: [:show]
-    def index
-      plants = Plant.all
-      render json: plants
-    end
-  
-    def show
-      plant = Plant.find(params[:id])
-      render json: plant
-    end
-  
-    def create
-      plant = Plant.create(plant_params)
-      render json: plant
-    end
-  
-    private
-  
-    def plant_params
-      params.require(:plant).permit(:name, :image, :price)
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find(params[:id])
+    render json: plant
+  end
+
+  def create
+    plant = Plant.new(plant_params)
+    if plant.save
+      render json: plant, status: :created
+    else
+      render json: plant.errors, status: :unprocessable_entity
     end
   end
-  
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :image, :price)
+  end
+end
